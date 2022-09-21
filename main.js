005149e548dba5af0bed806cec1113829e5a51ec
Parveshgit <108713587+Parveshgit@users.noreply.github.com>
2022-09-21 02:13:46 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,106 @@
+function calmercury(){
+    let weight = document.getElementById("hellohowareyou").value;
+    let mercuryw = weight * 0.38;
+    alert("Your weight on Mercury is : " + mercuryw);
+}
+
+function calvenus(){
+    let weight = document.getElementById("hellohowareyou").value;
+    let venusw = weight * 0.91;
+    alert("Your weight on Venus is : " + venusw);
+}
+
+function calearth(){
+    let weight = document.getElementById("hellohowareyou").value;
+    let earthw = weight * 1;
+    alert("Your weight on Earth is : " + earthw);
+}
+
+function calmars(){
+    let weight = document.getElementById("hellohowareyou").value;
+    let marsw = weight * 0.38;
+    alert("Your weight on Mars is : " + marsw);
+}
+
+function caljupiter(){
+    let weight = document.getElementById("hellohowareyou").value;
+    let jupiterw = weight * 2.34;
+    alert("Your weight on Jupiter is : " + jupiterw);
+}
+
+function calsaturn(){
+    let weight = document.getElementById("hellohowareyou").value;
+    let saturnw = weight * 1.06;
+    alert("Your weight on Saturn is : " + saturnw);
+}
+
+function caluranus() {
+    let weight = document.getElementById("hellohowareyou").value;
+    let uranusw = weight * 0.92;
+    alert("Your weight on Uranus is : " + uranusw);
+}
+
+function calneptune(){
+    let weight = document.getElementById("hellohowareyou").value;
+    let neptunew = weight * 1.19;
+    alert("Your weight on Neptune is : " + neptunew);
+}
+
+function calpluto(){
+    let weight = document.getElementById("hellohowareyou").value;
+    let plutow = weight * 0.06;
+    alert("Your weight on Pluto is : " + plutow);
+}
+
+// function calvenus(){
+//     let w2 = document.getElementById("hellohowareyou").value;
+//     let venusw = w2 * 0.91;
+//     alert("Your weight on venus is : " + venusw);
+// }
+
+// function calearth(){
+//     let w3 = document.getElementById("hellohowareyou").value;
+//     let venusw = w3 * 1;
+//     alert("Your weight on earth is : " + earthw);
+// }
+
+// function calmars(){
+//     let w3 = document.getElementById("hellohowareyou").value;
+//     let marsw = w3 * 0.38;
+//     alert("Your weight on Mars is : " + marsw);
+// }
+
+// function caljupiter(){
+//     let w4 = document.getElementById("hellohowareyou").value;
+//     let jupiterw = w4 * 2.34;
+//     alert("Your weight on Jupiter is : " + jupiterw);
+// }
+
+// function calsaturn(){
+//     let w5 = document.getElementById("hellohowareyou").value;
+//     let saturnw = w4 * 1.06;
+//     alert("Your weight on Saturn is : " + saturnw);
+// }
+// function Calculate() {
+//     let weight = document.getElementById("hellohowareyou").value;
+//     let mercuryw = weight * 0.38;
+//     let venusw = weight * 0.91;
+//     let earthw = weight * 1;
+//     let marsw = weight * 0.38;
+//     let jupiterw = weight * 2.34;
+//     let saturnw = weight * 1.06;
+//     let uranusw = weight * 0.92;
+//     let neptunew = weight * 1.19;
+//     let plutow = weight * 0.06;
+
+
+//     document.getElementById("mw").innerHTML = "your weight is:" + mercuryw;
+//     document.getElementById("vw").innerHTML = "your weight is:" + venusw;
+//     document.getElementById("ew").innerHTML = "your weight is:" + earthw;
+//     document.getElementById("msw").innerHTML = "your weight is:" + marsw;
+//     document.getElementById("jw").innerHTML = "your weight is:" + jupiterw;
+//     document.getElementById("sw").innerHTML = "your weight is:" + saturnw;
+//     document.getElementById("uw").innerHTML = "your weight is:" + uranusw;
+//     document.getElementById("nw").innerHTML = "your weight is:" + neptunew;
+//     document.getElementById("pw").innerHTML = "your weight is:" + plutow;
+// }
